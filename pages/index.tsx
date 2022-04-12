4c49fffad36b1586ee3a86fdbabb75ce1a2928f5
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-04-11 22:20:23 -0300

    chore(*): Change main files to TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { Header, Footer } from '../components';
+import { Header, Footer } from '@/components';
 
 export default function Home() {
   return (
