c268b1ec434207fdf4381031fccc9b0b7b017913
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-04-16 21:57:24 -0300

    feat(compoments): Add Dish-list

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ export * from './header';
 export * from './footer';
 export * from './page-template';
 export * from './dish';
+export * from './dish-list';
 export * from './search';
 export * from './character';
